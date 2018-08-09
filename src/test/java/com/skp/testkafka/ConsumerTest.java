794060757a3033d036d85617f3739c4ea2601fd1
th.yun <th.yun@sk.com>
2018-08-09 17:54:23 +0900

    process.conf 정의

--- src/test/java/com/skp/testkafka/ConsumerTest.java
+++ src/test/java/com/skp/testkafka/ConsumerTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.MockConsumer;
@@ -11,12 +13,15 @@ import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.common.TopicPartition;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.skp.testkafka.RunnableConsumer;
 import com.skp.testutil.ResourceHelper;
 import com.skp.testutil.ResourceHelper.LineReadCallback;
 
 public class ConsumerTest {
+	private static final Logger logger = LoggerFactory.getLogger(ConsumerTest.class);
 	MockConsumer<String, String> consumer;
 	
 	@Before
@@ -53,11 +58,6 @@ public class ConsumerTest {
 	}
 	
 	/*
-	Logstash grok pattern:
-	COMMONAPACHELOG %{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "(?:%{WORD:verb} %{NOTSPACE:request}(?: HTTP/%{NUMBER:httpversion})?|%{DATA:rawrequest})" %{NUMBER:response} (?:%{NUMBER:bytes}|-)
-	COMBINEDAPACHELOG %{COMMONAPACHELOG} %{QS:referrer} %{QS:agent}
-	ServiceMon expression:
-	%{ip:WORD}\s+%{identd:WORD}\s+%{userid:WORD}\s+\[%{date:DATE}\]\s+"%{request:TEXT}"\s+%{responseCode:WORD}\s+%{byteSent:NUMBER}\s+"%{referer:TEXT}"\s+"%{client:TEXT}"\s+"?%{responseTime:NUMBER}"?(?:$|\s.*)
 	*/
 	@Test
 	public void testConsumerAccessLog() throws IOException {
@@ -83,6 +83,60 @@ public class ConsumerTest {
 
 	    // Consume
 	    runnableConsumer.consume();
+	}
+	
+	/*
+	 * Logstash grok pattern: (https://github.com/elastic/logstash/blob/v1.4.2/patterns/grok-patterns)
+	 * COMMONAPACHELOG %{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "(?:%{WORD:verb} %{NOTSPACE:request}(?: HTTP/%{NUMBER:httpversion})?|%{DATA:rawrequest})" %{NUMBER:response} (?:%{NUMBER:bytes}|-)
+	 * COMBINEDAPACHELOG %{COMMONAPACHELOG} %{QS:referrer} %{QS:agent}
+	 * 
+	 * ServiceMon KeyType expression:
+	 * %{ip:WORD}\s+%{identd:WORD}\s+%{userid:WORD}\s+\[%{date:DATE}\]\s+"%{request:TEXT}"\s+%{responseCode:WORD}\s+%{byteSent:NUMBER}\s+"%{referer:TEXT}"\s+"%{client:TEXT}"\s+"?%{responseTime:NUMBER}"?(?:$|\s.*)
+	 *
+	 * Logmetric sample & pattern & output metric:
+	 * 10.202.212.58 - - [31/Jul/2018:17:48:29 +0900] "GET /assets/PMON_icon1-a6c18ea37d8809bb7521e9594e7e758e.png?20180528 HTTP/1.1" 200 2687 "http://pmon-dev.skplanet.com/hosts?f_field=hostname&f_service=&search=SMONi" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36" "0.010"
+     *
+	 * %{WORD:ip} %{WORD:identd} %{WORD:userid} \[%{DATE:date}\] \"%{WORD} %{WORD:request} %{WORD}\" %{LONG:responseCode} %{LONG:byteSent} \"%{DATA:referer}\" \"%{DATA:client}\" \"%{DOUBLE:responseTime}\"(?:$|\s.*)
+	 * (\S+) (\S+) (\S+) \[(.+?)\] \"(\S+) (\S+) (\S+)\" (\d+) (\d+) "(.*?)" "(.*?)" "([\d\.]+)"(?:$|\s.*)
+	 * 
+	 * {
+	 *   "type": "access",
+	 *   "count": 10,
+	 *   "responseCode.sum": 10000,
+	 *   "responseCode.min": 1000,
+	 *   "responseCode.max": 5000,
+	 *   "responseCode.200": 8,
+	 *   "responseCode.400": 1,
+	 *   "responseCode.500": 1,
+	 *   "byteSent.sum": 10000,
+	 *   "byteSent.min": 1000,
+	 *   "byteSent.max": 5000,
+	 *   "byteSent.1000": 8,
+	 *   "byteSent.10000": 1,
+	 *   "byteSent.100000": 1
+	 * }
+	 */
+	@Test
+	public void testPattern() throws IOException {
+		ResourceHelper.processResource("com/skp/testkafka/access.log", new LineReadCallback() {
+			@Override
+			public void processLine(String line) {
+				long count=0;
+				match(line);
+			} 
+		});
+	}
+	
+	private void match(String line) {
+		String regex = "(\\S+) (\\S+) (\\S+) \\[(.+?)\\] \\\"(\\S+) (\\S+) (\\S+)\\\" (\\d+) (\\d+) \"(.*?)\" \"(.*?)\" \"([\\d\\.]+)\"(?:$|\\s.*)";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher m = pattern.matcher(line);
+		if (m.find()) {
+			logger.debug("1=" + m.group(1) + ", 2=" + m.group(2) + ", 3=" + m.group(3) +
+					", 4=" + m.group(4) + ", 5=" + m.group(5) + ", 6=" + m.group(6) + ", 7=" + m.group(7));
+			logger.debug("8=" + m.group(8) + ", 9=" + m.group(9) + ", 10=" + m.group(10) + ", 11=" + m.group(11) + ", 12=" + m.group(12));
+		}
+		
 	}
 	
 }
